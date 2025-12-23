fb8c0be44e3bd500bdc50e4ad69d2fddc207b5f3
Emir Özbir <emirozbir@Emir-MacBook-Air.local>
2025-12-23 22:42:24 +0300

    feat/config-hash-calculation: Policy adjusted

--- pkg/webhook/mutation.go
+++ pkg/webhook/mutation.go
@@ -85,7 +85,7 @@ func createPatch(deployment *appsv1.Deployment, img SidecarImage, upstreamDNSAdd
 		Name:      sidecarName,
 		Image:     fmt.Sprintf("%s:%s", img.Name, img.Tag),
 		Resources: corev1.ResourceRequirements{},
-		Args:      []string{"-upstream", fmt.Sprintf("%s:%s", upstreamDNSAddress, "53")},
+		Args:      []string{"-upstream", fmt.Sprintf("%s:%s", upstreamDNSAddress, "53"), "-controller", "http://dns-mesh-controller-controller-manager-metrics-service:9442"},
 		Env:       envVars,
 	}
 
